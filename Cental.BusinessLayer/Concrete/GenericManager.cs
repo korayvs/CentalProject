01c24e3aed88e456c539e1f258d4fb3aafe660f8
Koray Vatansever <koray-vatansever@hotmail.com>
2025-01-02 22:18:18 +0300

    updated

---
+++ Cental.BusinessLayer/Concrete/GenericManager.cs
@@ -0,0 +1,50 @@
+﻿using Cental.BusinessLayer.Abstract;
+using Cental.DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cental.BusinessLayer.Concrete
+{
+    public class GenericManager<T> : IGenericService<T> where T : class
+    {
+        private readonly IGenericDal<T> _genericDal;
+
+        public GenericManager(IGenericDal<T> genericDal)
+        {
+            _genericDal = genericDal;
+        }
+
+        public void TCreate(T entity)
+        {
+            _genericDal.Create(entity);
+        }
+
+        public void TDelete(int id)
+        {
+            _genericDal.Delete(id);
+        }
+
+        public List<T> TGetAll()
+        {
+            return _genericDal.GetAll();
+        }
+
+        public T TGetById(int id)
+        {
+            var value = _genericDal.GetById(id);
+            if(value == null)
+            {
+                throw new Exception("Id'ye göre değer bulunamadı");
+            }
+            return value;
+        }
+
+        public void TUpdate(T entity)
+        {
+            _genericDal.Update(entity);
+        }
+    }
+}
