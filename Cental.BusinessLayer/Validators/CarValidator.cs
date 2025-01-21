b666b60228e5f1834b1b6106bbf308ff53310136
Koray Vatansever <koray-vatansever@hotmail.com>
2025-01-21 22:35:06 +0300

    19.5 update

---
+++ Cental.BusinessLayer/Validators/CarValidator.cs
@@ -0,0 +1,25 @@
+﻿using Cental.EntityLayer.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cental.BusinessLayer.Validators
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.ModelName).NotEmpty().WithMessage("Araba modeli boş bırakılamaz.");
+            RuleFor(c => c.Transmission).NotEmpty().WithMessage("Vites türü boş bırakılamaz.");
+            RuleFor(c => c.GasType).NotEmpty().WithMessage("Yakıt türü boş bırakılamaz.");
+            RuleFor(c => c.Price).NotEmpty().WithMessage("Fiyat boş bırakılamaz.");
+            RuleFor(c => c.Year).NotEmpty().WithMessage("Yıl boş bırakılamaz.");
+            RuleFor(c => c.Kilometer).NotEmpty().WithMessage("Km değeri boş bırakılamaz.");
+            RuleFor(c => c.GearType).NotEmpty().WithMessage("Vites boş bırakılamaz.");
+            RuleFor(c => c.SeatCount).NotEmpty().WithMessage("Koltuk sayısı boş bırakılamaz.");
+        }
+    }
+}
